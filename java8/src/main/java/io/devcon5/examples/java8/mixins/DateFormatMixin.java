82c908f4373ff117340a06c1dd65edeea8060c17
Gerald Mücke <gerald.muecke@devcon5.io>
2015-09-09 23:07:36 +0200

    - added date format mixin example

---
+++ java8/src/main/java/io/devcon5/examples/java8/mixins/DateFormatMixin.java
@@ -0,0 +1,40 @@
+package io.devcon5.examples.java8.mixins;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Mixin that provided a method returning a formatted date.
+ * It requires a method to be implemented that delivers a date-format pattern.
+ * This use case is somewhat theoretical but is only used to demonstrate how mixins work.
+ * Created by Gerald Mücke on 09.09.2015.
+ */
+public interface DateFormatMixin {
+
+    String getDateFormatPattern();
+
+    /**
+     * Default Method that is present on all instances implementing this interface
+     * @return
+     */
+    default String getFormattedDate() {
+
+        DateFormat format = new SimpleDateFormat(getDateFormatPattern());
+        return format.format(getThisDate());
+    }
+
+    /**
+     * Default method to return the date.
+     * @return
+     * If the implementing class is of type date, it is returned, otherwise
+     * the current date is returned.
+     */
+    default  Date getThisDate() {
+        if(this instanceof Date) {
+            return (Date) this;
+        } else {
+            return new Date();
+        }
+    }
+}
